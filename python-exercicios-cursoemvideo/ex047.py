c7b62fcb528cba5c233ce85862efa9c4de1ecaac
José Brenon <eibrenon88@gmail.com>
2023-08-10 00:22:04 -0300

    Adicionando o exercício 047 do curso de Python

---
+++ python-exercicios-cursoemvideo/ex047.py
@@ -0,0 +1,14 @@
+"""
+    Esse exercício é referente a aula 13 do curso de Python do Curso em vídeo:
+
+    Desafio 047: Crie um programa que mostre na tela todos
+    os números pares que estão no intervalo entre 1 e 50.
+
+    Esse programa verifica se o número que está entre 1 e 50 é par
+    e mostra somente os que são par.
+
+    Autor: José Brenon - 09/08/2023
+"""
+for c in range(2, 51, 2):
+    print(c, end=' ')
+print("FIM")
