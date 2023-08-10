69d946261d20777c588e0fc61a45a92b166633a9
José Brenon <eibrenon88@gmail.com>
2023-08-10 00:48:21 -0300

    Adicionando o exercício 049 do curso de Python

---
+++ python-exercicios-cursoemvideo/ex049.py
@@ -0,0 +1,13 @@
+"""
+    Esse exercício é referente a aula 13 do curso de Python do Curso em vídeo:
+
+    Desafio 048: Refaça o DESAFIO 9, mostrando a tabuada de um
+    número que o usuário escolher, só que agora utilizando um laço for.
+
+    Esse programa pede um número inteiro e usando o laço for mostra a sua tabuada.
+
+    Autor: José Brenon - 09/08/2023
+"""
+n = int(input('Digite um número para saber a sua tabuada: '))
+for c in range(1, 11):
+    print('{} x {:2} = {:2}'.format(n, c, (n * c)))
