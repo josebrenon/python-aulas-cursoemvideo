b1282d99d47ba8e8ab4afaf7cb03acc1821d1268
José Brenon <eibrenon88@gmail.com>
2023-10-05 15:14:02 -0300

    Add o ex 085 em Python

---
+++ python-exercicios-cursoemvideo/ex085.py
@@ -0,0 +1,12 @@
+"""
+    Esse exercício é referente a aula 18 do curso de Python do Curso em vídeo:
+
+    Desafio 085:Crie um programa onde o usuário possa digitar sete valores 
+    numéricos e cadastre-os em uma lista única que mantenha separados os 
+    valores pares e ímpares. No final, mostre os valores pares e 
+    ímpares em ordem crescente.
+
+    Este programa 
+
+    Autor: José Brenon - 02/10/2023
+"""
