471aeed1004023ba06df0f70821aeca66fd50606
José Brenon <eibrenon88@gmail.com>
2023-10-06 16:38:41 -0300

    Finalizando o ex085 em Python

--- python-exercicios-cursoemvideo/ex085.py
+++ python-exercicios-cursoemvideo/ex085.py
@@ -6,7 +6,23 @@
     valores pares e ímpares. No final, mostre os valores pares e 
     ímpares em ordem crescente.
 
-    Este programa 
+    Este programa pede ao usuário para digitar 7 valores, analisa quais deles
+    são pares e quais são impares, se for par coloca no index 0 da lista e se for
+    impar coloca no index 1, depois organiza cada uma das listas separadamente 
+    e mostra os resultados.
 
     Autor: José Brenon - 02/10/2023
 """
+num = [[],[]]
+valor = 0
+for i in range(1, 8):
+    valor = int(input(f'Digite o {i}º valor: '))
+    if valor %2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+num[0].sort()
+num[1].sort()
+print('-='*40)
+print(f'Os valores pares digitados foram {num[0]}')
+print(f'Os valores impares digitados foram {num[1]}')
